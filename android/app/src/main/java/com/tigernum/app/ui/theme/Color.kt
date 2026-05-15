66ae725c4acfb94f8d7aa4ed127ca1b8ec942fec
9i89oo29-source <9i89oo29@gmail.com>
2026-05-15 11:18:00 +0300

    Create Color.kt

---
+++ android/app/src/main/java/com/tigernum/app/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.tigernum.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF8A1538)
+val PrimaryDark = Color(0xFF6A0F2A)
+val OnPrimary = Color(0xFFFFFFFF)
+val Secondary = Color(0xFF007A33)
+val OnSecondary = Color(0xFFFFFFFF)
+val Background = Color(0xFFFFFBF7)
+val Surface = Color(0xFFFFFFFF)
+val OnBackground = Color(0xFF1C1B1F)
+val OnSurface = Color(0xFF1C1B1F)
