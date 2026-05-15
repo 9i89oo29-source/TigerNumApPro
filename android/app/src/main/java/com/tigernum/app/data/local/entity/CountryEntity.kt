93621a47fc8166a3dda8d6aec051782af28ceb09
9i89oo29-source <9i89oo29@gmail.com>
2026-05-15 10:49:54 +0300

    Create CountryEntity.kt

---
+++ android/app/src/main/java/com/tigernum/app/data/local/entity/CountryEntity.kt
@@ -0,0 +1,12 @@
+package com.tigernum.app.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "countries")
+data class CountryEntity(
+    @PrimaryKey val code: String,
+    val name: String,
+    val flag: String,
+    val dialCode: String
+)
